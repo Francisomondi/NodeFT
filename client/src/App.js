5a70f3afb893e67c589008c8c29527dcc7743b19
francis omondi <francisomondi17@gmail.com>
2022-11-11 04:04:07 +0300

    frontend form

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,26 @@
 import './App.css';
 
 function App() {
-  return <div className = "App" > kjdfvhkjdsfhkj < /div>
+  return <div className = "App" >
+    <
+    label > Name < /label> <
+  input type = "text" / >
+    <
+    label > age < /label>  <
+  input type = "number" / >
+    <
+    label > country < /label> <
+  input type = "text" / >
+
+    <
+    label > country < /label> <
+  input type = "text" / >
+    <
+    label > wages(per year) < /label> <
+  input type = "number" / >
+
+    <
+    /div>
 
 }
 
