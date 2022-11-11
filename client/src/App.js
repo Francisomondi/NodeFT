010db7b55d78ea46c91cec069547cbcaa6adcd71
francis omondi <francisomondi17@gmail.com>
2022-11-11 04:37:40 +0300

    form styling

--- client/src/App.js
+++ client/src/App.js
@@ -3,21 +3,25 @@ import './App.css';
 function App() {
   return <div className = "App" >
     <
-    label > Name < /label> <
-  input type = "text" / >
+    div className = "information" >
     <
-    label > age < /label>  <
-  input type = "number" / >
+    label > Name < /label> <input type = "text" / >
     <
-    label > country < /label> <
-  input type = "text" / >
+    label > age < /label>  <input type = "number" / >
+    <
+    label > country < /label> <input type = "text" / >
+    <
+    label > country < /label> <input type = "text" / >
+    <
+    label > wages(per year) < /label> <input type = "number" / >
 
     <
-    label > country < /label> <
-  input type = "text" / >
-    <
-    label > wages(per year) < /label> <
-  input type = "number" / >
+    button > Add employee < /button> <
+    /div>
+
+
+
+
 
     <
     /div>
