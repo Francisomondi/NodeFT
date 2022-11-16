7236c6a2c1877ee115d948c3c532123a318ea5ab
francis omondi <francisomondi17@gmail.com>
2022-11-16 03:28:13 +0300

    modify client beautify extension for prettier code

--- client/src/App.js
+++ client/src/App.js
@@ -15,32 +15,26 @@ function App() {
   const displayResults = () => {
     console.log(name + age + country + position + wage);
   }
-  return <div className = "App" >
-    <
-    div className = "information" >
-    <
-    label > Name < /label> <input type = "text" onChange={(event)=>{setName(event.target.value)}} / >
-    <
-    label > age < /label>  <input type = "number" onChange={(event)=>{setAge(event.target.value)}}/ >
-    <
-    label > country < /label> <input type = "text"  onChange={(event)=>{setCountry(event.target.value)}}/ >
-    <
-    label > position < /label> <input type = "text" onChange={(event)=>{setPosition(event.target.value)}} / >
-    <
-    label > wages(per year) < /label> <input type = "number" onChange={(event)=>{setWage(event.target.value)}}/ >
+  return <div className="App" >
+    <div className="information">
 
-    <
-    button onClick = {
-      displayResults
-    } > Add Employee < /button> < /
-  div >
+      <label> Name </label> <input type="text" onChange={(event) => { setName(event.target.value) }} />
+      <label>age</label>    <input type="number" onChange={(event) => { setAge(event.target.value) }} />
+      <label> country </label> <input type="text" onChange={(event) => { setCountry(event.target.value) }} />
+      <label> position </label> <input type="text" onChange={(event) => { setPosition(event.target.value) }} />
+      <label > wages(per year) </label> <input type="number" onChange={(event) => { setWage(event.target.value) }} />
+
+      <button onClick={
+        displayResults
+      } > Add Employee </button>
+
+    </div>
 
 
 
 
 
-    <
-    /div>
+  </div>
 
 }
 
