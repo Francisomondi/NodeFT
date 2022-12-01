b87e8dffa3705093c6d2a0b776ecdb19bc97a468
francis omondi <francisomondi17@gmail.com>
2022-12-01 08:02:00 +0300

    creating and setting up the database

--- server/index.js
+++ server/index.js
@@ -1,5 +1,14 @@
 const express = require("express")
 const app = express()
+const mysql = require("mysql")
+
+const db = mysql.createConnection({
+    user: "root",
+    host: "localhost",
+    password: "1234",
+    database: "employeesystem"
+})
+
 
 const PORT = 5000
 app.listen(PORT, () => console.log(`listening on port ${PORT}`))
