5c396a2331c65410094a30c0efa37b81df27eb27
francis omondi <francisomondi17@gmail.com>
2022-12-05 11:49:31 +0300

    update before continuation

--- server/index.js
+++ server/index.js
@@ -9,6 +9,22 @@ const db = mysql.createConnection({
     database: "employeesystem"
 })
 
+app.post("/create", (req, res) => {
+    const name = req.body.name,
+    const age = req.body.age,
+    const country = req.body.country,
+    const position = req.body.position,
+    const wages = req.body.wages
+
+    db.query("INSERT INTO employees(name,age,country,position,wages) VALUES(?,?,?,?,?)",
+        [nam, eage, country, position, wages], (err, result) => {
+            if (err) console.log(err)
+            else (
+                res.send("values inserted successfully")
+            )
+
+        })
+})
 
 const PORT = 5000
 app.listen(PORT, () => console.log(`listening on port ${PORT}`))
